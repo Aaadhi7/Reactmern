1a1d5bf9d6c7c872f9cf86b03dbe41f5cd866347
adithyan <adithyanpradeep36@gmail.com>
2024-02-29 11:01:21 +0530

    react

--- server1/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require ('express')
-const router = express.Router();
-const authController = require('../controller/authController');
-const checkLogin = require('../utils/checkLogin').checkLogin
-
-router.post('/login',authController.login);
-
-module.exports = router;
